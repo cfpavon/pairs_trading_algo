64ea9313bc52028f5ea199e97eca3086493c8911
Carlos F. Pavon <34008901+cfpavon@users.noreply.github.com>
2021-08-01 08:22:23 -0600

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,18 +2,13 @@
 
 
 
-#account_id="cfpavon"
-#password="Cafer#4664!"
-#api_key="2a47939b5f469133dc59ca17143533c1adefa3d5"
-#environment="LIVE"
-
 #watchlist_id="14233239"
 
 
-account_id="cpavon31"
-acc_password="Cafer4777"
-api_key="d30a3f30272ae879465252eab6fa6fdb1d23ceb2"
-acc_environment="DEMO"
+account_id=""
+acc_password=""
+api_key=""
+acc_environment=""
 
 watchlist_id="15401213"
 
